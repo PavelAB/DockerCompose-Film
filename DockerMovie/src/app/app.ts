190711920605b77f855c1c5413265e153003eb2e
Pavel Bezukladnikov <bezukladnikov.pavel.be@gmail.com>
2025-11-24 12:12:42 +0100

    feat: connection DB - Back works

--- DockerMovie/src/app/app.ts
+++ DockerMovie/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieComponent } from "./features/movie.component/movie.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MovieComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- back/Controllers/MovieController.cs
+++ back/Controllers/MovieController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Data.Common;
+using System;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 
 namespace FilmApiPostgreSQL.Controllers
 {
@@ -18,9 +22,33 @@ namespace FilmApiPostgreSQL.Controllers
         {
             try
             {
-                _connection.Open();
-                return Ok(new { message="test"});
+                if (_connection is null)
+                    throw new Exception("_connection is NUll");
+
+                var films = new List<object>();
+
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM film;", (NpgsqlConnection)_connection))
+                {
+                    _connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var film = new
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("id")),
+                                Titre = reader.GetString(reader.GetOrdinal("titre")),
+                                TitreCA = reader.GetString(reader.GetOrdinal("titreca")),
+                                Annee = reader.GetDateTime(reader.GetOrdinal("annee"))
+                            };
+
+                            films.Add(film);
+                        }
+                        return Ok(films);
+                    }
+                }
             }
+
             catch (Exception Ex)
             {
 
